271cad407d6a9ca4380e9e2f2e8c47a6414d2528
Innpaul Zhang <zyp108421@gmail.com>
2020-04-24 00:54:51 +0800

    fix case 03

--- 03-udp-magic.js
+++ 03-udp-magic.js
@@ -2,6 +2,6 @@ const dgram = require('dgram')
 const naive = require('./lib/naive-protocol')
 
 const client = dgram.createSocket('udp4')
-client.send(Buffer.concat([Buffer.from('hello'), naive('world')]), 1234, '127.0.0.1', () => {
+client.send(Buffer.concat([Buffer.from('naive'), naive('hello world')]), 1235, '127.0.0.1', () => {
   client.close()
 })
